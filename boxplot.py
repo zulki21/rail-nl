c7fae46608a8b9add3c3a80030fedcf072f98515
root <aruesink13@gmail.com>
2022-01-20 13:35:53 +0100

    initial boxplot

--- boxplot.py
+++ boxplot.py
@@ -1,1 +1,17 @@
 import matplotlib.pyplot as plt
+from algorunner import *
+
+Runs = AlgoRunner(1000)
+
+box_data = Runs.algo_samples
+
+k_values = []
+
+for i in range(len(box_data)):
+    
+    k = box_data[i].get_k()
+    k_values.append(k)
+
+plt.boxplot(k_values,patch_artist=True,labels=['random'])
+plt.savefig("boxplot.png", format = "png")
+
