ed4c211e2b3de01bc470b9ff87d1a3c81b005b89
Robert <ryoussef0103@hotmail.com>
2022-01-20 13:50:00 +0100

    barchart added

--- main2.py
+++ main2.py
@@ -1,8 +1,35 @@
 from algorunner import AlgoRunner
 from randomalgo import RandomAlgo
 from algorunner import AlgoRunner
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plotBar():
+    plt.figure(figsize=(20, 10), dpi=80)
+
+    # Make a random dataset:
+    height = a.barChart()
+    bars = list(map(str, list(range(len(height)))))
+    y_pos = np.arange(len(bars))
+
+    # Create bars
+    plt.bar(y_pos, height, width=0.9)
+
+    # Create names on the x-axis
+    plt.xticks(np.arange(0, len(bars)+1, 25))
+
+    plt.gca().invert_yaxis()
+
+    # Show graphic
+    plt.show()
+    plt.savefig('barChart.png')
+
+
 if __name__ in '__main__':
-    a = AlgoRunner(1000) 
+    a = AlgoRunner(100)
 
     print(a.max_K())
     print(a.stats())
+
+    plotBar()
