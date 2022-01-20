fda9a0c1ea03a7d63ecab036c4a0cb30c44af1b7
Robert <ryoussef0103@hotmail.com>
2022-01-20 16:22:26 +0100

    k en N verwisseld

--- main2.py
+++ main2.py
@@ -6,22 +6,28 @@ import numpy as np
 
 
 def plotBar():
-    plt.figure(figsize=(20, 10), dpi=80)
 
-    # Make a random dataset:
+    # Create figure
+    # plt.figure(figsize=(20, 10), dpi=80)
+
+    # Adding data
     height = a.barChart()
     bars = list(map(str, list(range(len(height)))))
     y_pos = np.arange(len(bars))
 
     # Create bars
-    plt.bar(y_pos, height, width=0.9)
+    plt.barh(height, y_pos)
 
-    # Create names on the x-axis
+    # N on the x-axis with a frequency of 25
     plt.xticks(np.arange(0, len(bars)+1, 25))
 
-    plt.gca().invert_yaxis()
+    # Labels for the graph
+    plt.xlabel("K-waarden")
+    plt.ylabel("N")
 
-    # Show graphic
+    # plt.gca().invert_xaxis()
+
+    # Show and save graphic
     plt.show()
     plt.savefig('barChart.png')
 
@@ -31,5 +37,6 @@ if __name__ in '__main__':
 
     print(a.max_K())
     print(a.stats())
+    print(a.barChart())
 
     plotBar()
