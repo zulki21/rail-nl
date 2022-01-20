c9635b945fa4d3d5b57ee3313b3d6ac520275cf7
Robert <ryoussef0103@hotmail.com>
2022-01-20 16:26:55 +0100

    niets veranderd

--- randomalgo.py
+++ randomalgo.py
@@ -3,6 +3,7 @@ from loader import load_stations
 import random
 from connection import Train
 
+
 def unique_connection_train(train):
     """
     finds all unique connections of this train
@@ -69,6 +70,8 @@ def total_time_trains(trains):
         min += train.get_time_route()
 
     return min
+
+
 def check_if_contains(all_connections, set):
     if len(all_connections) == 0:
         return False
@@ -77,6 +80,8 @@ def check_if_contains(all_connections, set):
             if set == connection:
                 return True
     return False
+
+
 class RandomAlgo:
     def __init__(self) -> None:
         self.stations = load_stations()
@@ -87,12 +92,11 @@ class RandomAlgo:
         # creates object with all connections
         for station in self.stations.values():
             for connection in list(station.connections.keys()):
-                if check_if_contains(self.all_connections, {station,connection}) == False:
+                if check_if_contains(self.all_connections, {station, connection}) == False:
                     self.all_connections.append({station, connection})
 
-
     # adding routes to the trains randomly
-        for i in range(random.randint(0,7)):
+        for i in range(random.randint(0, 7)):
             starting_station = random.choice(list(self.stations.values()))
 
             self.trains.append(Train(starting_station))
@@ -100,7 +104,6 @@ class RandomAlgo:
         for train in self.trains:
             current_station = train.get_route()[0]
             while train.get_time_route() < 120:
-                 
 
                 connections = list(current_station.get_connections().keys())
                 potential_connections = []
@@ -109,33 +112,30 @@ class RandomAlgo:
                     if check_if_contains(self.used_connections, {current_station, next_station}) == False:
                         potential_connections.append(next_station)
 
-
                 if len(potential_connections) != 0:
                     next_station = random.choice(potential_connections)
                 else:
                     next_station = random.choice(connections)
 
                 train.add_station(next_station)
-                if check_if_contains(self.used_connections,{current_station,next_station}) == False:
-                    self.used_connections.append({current_station,next_station})
+                if check_if_contains(self.used_connections, {current_station, next_station}) == False:
+                    self.used_connections.append(
+                        {current_station, next_station})
                 current_station = next_station
-        
-        
+
         first_tuple_list = [tuple(lst) for lst in self.all_connections]
         secnd_tuple_list = [tuple(lst) for lst in self.used_connections]
 
         first_set = set(first_tuple_list)
         secnd_set = set(secnd_tuple_list)
-        diff = first_set.symmetric_difference(secnd_set) 
-        
+        diff = first_set.symmetric_difference(secnd_set)
 
         while len(diff) > 0 and len(self.trains) <= 7:
             starting_station = random.sample(diff, 1)[0][0]
             self.trains.append(Train(starting_station))
-            train  = self.trains[-1]
+            train = self.trains[-1]
             current_station = train.get_route()[0]
             while train.get_time_route() < 120:
-                 
 
                 connections = list(current_station.get_connections().keys())
                 potential_connections = []
@@ -150,21 +150,20 @@ class RandomAlgo:
                     next_station = random.choice(connections)
 
                 train.add_station(next_station)
-                if check_if_contains(self.used_connections,{current_station,next_station}) == False:
-                    self.used_connections.append({current_station,next_station})
+                if check_if_contains(self.used_connections, {current_station, next_station}) == False:
+                    self.used_connections.append(
+                        {current_station, next_station})
                 current_station = next_station
-
 
             first_tuple_list = [tuple(lst) for lst in self.all_connections]
             secnd_tuple_list = [tuple(lst) for lst in self.used_connections]
 
             first_set = set(first_tuple_list)
             secnd_set = set(secnd_tuple_list)
-            diff = first_set.symmetric_difference(secnd_set) 
+            diff = first_set.symmetric_difference(secnd_set)
 
             if len(self.used_connections) != 28:
-                self.reset() 
-    
+                self.reset()
 
     def reset(self):
         self.__init__()
@@ -177,7 +176,3 @@ class RandomAlgo:
 
         K = 10000 * (a / 28) - (len(self.trains) * 100 + min)
         return K
-
-
-
-
