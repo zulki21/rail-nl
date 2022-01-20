a00d545e0710d037db1afed7b8a94d68d0fef7fe
Zulkarneym <zulkarneyn@Desktop-NR200.localdomain>
2022-01-20 15:19:32 +0100

    added constraint all lines and time constraint

--- randomalgo.py
+++ randomalgo.py
@@ -1,3 +1,4 @@
+from tracemalloc import start
 from loader import load_stations
 import random
 from connection import Train
@@ -68,36 +69,115 @@ def total_time_trains(trains):
         min += train.get_time_route()
 
     return min
-
+def check_if_contains(all_connections, set):
+    if len(all_connections) == 0:
+        return False
+    else:
+        for connection in all_connections:
+            if set == connection:
+                return True
+    return False
 class RandomAlgo:
     def __init__(self) -> None:
         self.stations = load_stations()
-
         self.trains = []
+        self.used_connections = []
+        self.all_connections = []
+
+        # creates object with all connections
+        for station in self.stations.values():
+            for connection in list(station.connections.keys()):
+                if check_if_contains(self.all_connections, {station,connection}) == False:
+                    self.all_connections.append({station, connection})
+
 
     # adding routes to the trains randomly
-        for i in range(random.randint(0,100)):
+        for i in range(random.randint(0,7)):
             starting_station = random.choice(list(self.stations.values()))
 
             self.trains.append(Train(starting_station))
 
         for train in self.trains:
             current_station = train.get_route()[0]
-            random_num = random.randint(0, 120)
-            while train.get_time_route() < random_num:
+            while train.get_time_route() < 120:
+                 
+
                 connections = list(current_station.get_connections().keys())
-                next_station = random.choice(connections)
+                potential_connections = []
+
+                for next_station in connections:
+                    if check_if_contains(self.used_connections, {current_station, next_station}) == False:
+                        potential_connections.append(next_station)
+
+
+                if len(potential_connections) != 0:
+                    next_station = random.choice(potential_connections)
+                else:
+                    next_station = random.choice(connections)
+
                 train.add_station(next_station)
+                if check_if_contains(self.used_connections,{current_station,next_station}) == False:
+                    self.used_connections.append({current_station,next_station})
                 current_station = next_station
+        
+        
+        first_tuple_list = [tuple(lst) for lst in self.all_connections]
+        secnd_tuple_list = [tuple(lst) for lst in self.used_connections]
+
+        first_set = set(first_tuple_list)
+        secnd_set = set(secnd_tuple_list)
+        diff = first_set.symmetric_difference(secnd_set) 
+        
+
+        while len(diff) > 0 and len(self.trains) <= 7:
+            starting_station = random.sample(diff, 1)[0][0]
+            self.trains.append(Train(starting_station))
+            train  = self.trains[-1]
+            current_station = train.get_route()[0]
+            while train.get_time_route() < 120:
+                 
+
+                connections = list(current_station.get_connections().keys())
+                potential_connections = []
+
+                for next_station in connections:
+                    if check_if_contains(self.used_connections, {current_station, next_station}) == False:
+                        potential_connections.append(next_station)
+
+                if len(potential_connections) != 0:
+                    next_station = random.choice(potential_connections)
+                else:
+                    next_station = random.choice(connections)
+
+                train.add_station(next_station)
+                if check_if_contains(self.used_connections,{current_station,next_station}) == False:
+                    self.used_connections.append({current_station,next_station})
+                current_station = next_station
+
+
+            first_tuple_list = [tuple(lst) for lst in self.all_connections]
+            secnd_tuple_list = [tuple(lst) for lst in self.used_connections]
+
+            first_set = set(first_tuple_list)
+            secnd_set = set(secnd_tuple_list)
+            diff = first_set.symmetric_difference(secnd_set) 
+
+            if len(self.used_connections) != 28:
+                self.reset() 
     
+
+    def reset(self):
+        self.__init__()
+
     def get_k(self):
         # calculate k value of the given random run
         a = unique_total_connections(self.trains)
 
         min = total_time_trains(self.trains)
 
-        K = 10000 * (a / 89) - (len(self.trains) * 100 + min)
+        K = 10000 * (a / 28) - (len(self.trains) * 100 + min)
         return K
+
 
 
 
