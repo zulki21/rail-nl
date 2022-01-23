84fe297efacbcec84cf710a0e5e02f52c798b470
Robert <ryoussef0103@hotmail.com>
2022-01-23 16:42:46 +0100

    commit

--- boxplot.py
+++ boxplot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from algorunner import *
+import numpy as np
 
 Runs = AlgoRunner(1000)
 
@@ -8,10 +9,11 @@ box_data = Runs.algo_samples
 k_values = []
 
 for i in range(len(box_data)):
-    
+
     k = box_data[i].get_k()
     k_values.append(k)
 
-plt.boxplot(k_values,patch_artist=True,labels=['random'])
-plt.savefig("boxplot.png", format = "png")
-
+plt.boxplot(k_values, patch_artist=True, labels=['random'])
+# plt.yticks(np.arange(8000, 9100, 100))
+# plt.ylim([8000, 9000])
+plt.savefig("boxplot.png", format="png")
