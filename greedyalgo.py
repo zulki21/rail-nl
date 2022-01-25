cb4a09ab119c2d948002d19b1be6e2f396397344
root <aruesink13@gmail.com>
2022-01-25 14:29:19 +0100

    final_k added

--- greedyalgo.py
+++ greedyalgo.py
@@ -67,10 +67,10 @@ class GreedyAlgo:
                     self.used_connections.append({current_station, next_station})
                 current_train.add_station(next_station)
             
-        print(get_k(len(self.trains), (len(self.used_connections)), total_time_trains(self.trains) + connection.get_time(current_station)))
             
+    def final_k(self):
+
+        return 10000 * (len(self.used_connections) / 28) - (len(self.trains) * 100 + total_time_trains(self.trains))
+
+
                     
-
-
-a = GreedyAlgo()
-
