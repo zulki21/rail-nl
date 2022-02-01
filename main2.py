179983301c1fc1e641628abaae7027918945bb6f
Robert <ryoussef0103@hotmail.com>
2022-02-01 12:27:14 +0100

    functions

--- main2.py
+++ main2.py
@@ -4,14 +4,18 @@ from code.algorithmRunner.algorunner import AlgoRunner
 from code.algorithmRunner.hillclimberrunner import AlgoRunnerHill
 from code.algorithms.randomalgo import RandomAlgo
 from code.algorithmRunner.greedyrunner import GreedyRunner
-from code.algorithms.greedyalgo import *
-from code.visualization.visualize import visualize_boxplot_Random, visualize_boxplot_Greedy, randomHist, greedyHist, createTabelGreedy, createTabelRandom
+from code.algorithms.greedyalgo import GreedyAlgo
+
+from code.visualization.visualize import visualize_boxplot_Random, visualize_boxplot_Greedy, randomHist, greedyHist, createTabelGreedy, createTabelRandom, hillclimberHist, createTabelHillclimber, visualize_boxplot_Hillclimber
 
 
 if __name__ in '__main__':
     # visualize_boxplot_Random()
     # visualize_boxplot_Greedy()
+    visualize_boxplot_Hillclimber()
     # greedyHist()
     # randomHist()
-    createTabelRandom()
-    createTabelGreedy()
+    hillclimberHist()
+    # createTabelRandom()
+    # createTabelGreedy()
+    createTabelHillclimber()
