591e905c483108bd12f4635da51fa5ae77381f82
zulki21 <zulkarneyn@Desktop-NR200.localdomain>
2022-02-02 23:36:30 +0100

    added comments to all of hillclimber

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -5,6 +5,18 @@ import random
 
 
 def total_time_trains(trains):
+    """
+    returns total time of all the trains
+
+    Parameters
+    ----------
+    trains: list of train objects
+
+    Returns   
+    ------
+    int
+        time in mintues
+    """
     min = 0
     for train in trains:
         min += train.get_time_route()
@@ -13,6 +25,20 @@ def total_time_trains(trains):
 
 
 def check_if_contains(all_connections, set):
+    """
+    checks if a particular connections exist in a list of connections
+
+    Parameters
+    ----------
+    all_connections: list of sets
+        a list of sets containing all connections as sets of 2 stations
+    set: set
+        a set containing 2 station objects
+    Returns
+    ------
+    boolean
+        returns true or false
+    """
     if len(all_connections) == 0:
         return False
     else:
@@ -24,8 +50,45 @@ def check_if_contains(all_connections, set):
 
 class Hillclimber:
     def __init__(self, region, reset_bound) -> None:
+        """
+        A class used to represent a Station
 
-        # Run randomalgo for a random state
+        ...
+
+        Attributes
+        ----------
+        region : int
+            a integer which represent 2 regions Holland or Nationaal
+        stations : dict of station objects
+            dictionary containing station names and the object
+        trains: list
+            list containing train objects 
+        used_connections : list of sets
+            list which keeps track of used connections
+        all_connections : list of sets
+            list with all connections
+        best_used_connections : list of sets
+            list with best connections the algorithm has found during process
+        best_trains : list of train object
+            list of best trains it has found
+        highest_k: int
+            highest k value during process
+        max_time: int
+            integer which keeps track of maximum time a train may use
+        max_trains : int
+            maximum amount of trains total model can use
+        mistake_counter : int
+            keeps track of how many times algorithm has failed to produce better output
+        Methods
+        -------
+        random_change()
+            performs a random change on a random state space
+        get_k()
+            returns k value of a state space
+        get_trains()
+            returns list of trains
+        """
+
         a = RandomAlgo(region)
         self.region = region
         self.mistake_bound = reset_bound
@@ -42,6 +105,7 @@ class Hillclimber:
 
         self.mistake_counter = 0
 
+        # check which region we are using change values accordingly
         if self.region == 1:
             self.number_connection = 28
             self.max_time = 120
@@ -51,10 +115,12 @@ class Hillclimber:
             self.max_time = 180
             self.max_trains = 20
 
+        # keep algorithm running untill no improvement takes place after certain steps
         while self.mistake_counter < self.mistake_bound:
-
+            # make small random change
             self.random_change()
 
+            # store variables if we find higher k
             if self.get_k() > self.highest_k:
                 self.mistake_counter = 0
                 self.best_used_connections = self.used_connections[:]
@@ -66,15 +132,16 @@ class Hillclimber:
                 self.mistake_counter += 1
 
     def random_change(self):
-        # train we want to change
-        train = random.choice(self.trains)
-        # delete train
-        self.trains.remove(train)
+        """
+        applies a random change to a state space
+        """
+
+        # remove 3 trains
+        self.trains.remove(random.choice(self.trains))
         self.trains.remove(random.choice(self.trains))
         self.trains.remove(random.choice(self.trains))
 
         # recalculate used connections
-
         unique_pairs_alpha = []
 
         combis = []
@@ -104,13 +171,17 @@ class Hillclimber:
 
         self.used_connections = b_set
 
+        # look for unused conncetions
         first_tuple_list = [tuple(lst) for lst in self.all_connections]
         secnd_tuple_list = [tuple(lst) for lst in self.used_connections]
 
         first_set = set(first_tuple_list)
         secnd_set = set(secnd_tuple_list)
         diff = first_set.symmetric_difference(secnd_set)
+    
 
+
+        
         while len(diff) > 0 and len(self.trains) < self.max_trains:
             starting_station = random.sample(diff, 1)[0][0]
             self.trains.append(Train(starting_station))
@@ -146,15 +217,28 @@ class Hillclimber:
             diff = first_set.symmetric_difference(secnd_set)
 
     def get_k(self):
-        # calculate k value of the given random run
+        """
+        returns k value for a given state space
+
+        Returns
+        -------
+        int
+            K-value
+        """
         a = len(self.used_connections)
         min = total_time_trains(self.trains)
         K = 10000 * (a / self.number_connection) - \
             (len(self.trains) * 100 + min)
         return K
 
-    def get_unused(self):
-        return list(set([frozenset(i) for i in self.all_connections]) - set([frozenset(i) for i in self.used_connections]))
 
     def get_trains(self):
+        """
+        returns list of trains
+
+        Returns
+        -------
+        list
+            list of trains object
+        """
         return self.trains
